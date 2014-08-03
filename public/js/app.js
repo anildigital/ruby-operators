be3e6ca2a4daa1fb08b57e63e718e4fdb6d8c837
Anil Wadghule <anildigital@gmail.com>
2014-08-03 15:09:11 +0530

    Use pjax like urls. Use angular routing. Some ❤️.

--- public/js/app.js
+++ public/js/app.js
@@ -1,39 +1,17 @@
-function App($scope, $http) {
+var RubyOperatorsApp = angular.module('RubyOperatorsApp', [
+    'ngRoute',
+    'rubyOperatorsAppControllers',
+    'rubyOperatorsAppServices'
+]);
 
-    $scope.select = function(selectedOperator) {
-        $scope.selectedOperator = selectedOperator;
-    };
-
-    $scope.operatorList = [];
-
-    $http.get('config/operators.json').success(function(data) {
-        $scope.operatorList = data.operators;
-        $scope.selectedOperator = $scope.operatorList[1];
-    });
-
-    $scope.navClass = function (operator) {
-        return operator === $scope.selectedOperator ? 'active' : '';
-    };   
-
-    $scope.selectedOperator = $scope.operatorList[4];
-
-    $scope.showNextOperator = function() {
-        if (screen.width < 760){
-            var index = $scope.operatorList.indexOf($scope.selectedOperator);
-            if (index === ($scope.operatorList.length - 1)) {
-                $scope.selectedOperator = $scope.operatorList[0];
-            }
-            else {
-                $scope.selectedOperator = $scope.operatorList[index+1];
-            }
-        }
-    };
-
-    $scope.$watch(function() {
-      return document.getElementById("code_example");
-    },
-    function() {
-      hljs.highlightBlock(document.getElementById("code_example"));
-    });
-
-}
+RubyOperatorsApp.config(['$routeProvider', '$locationProvider', '$anchorScrollProvider', function($routeProvider, $locationProvider, $anchorScrollProvider ) {
+    $anchorScrollProvider.disableAutoScrolling(); 
+    $locationProvider.html5Mode(true);
+    $routeProvider.
+        when('/:operatorName', {
+            templateUrl: 'app.html',
+            controller: 'SingleRubyOperatorCtrl'
+        }).otherwise({
+            redirectTo: '/spaceship'
+        });
+}]);
