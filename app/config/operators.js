2607a87c1ea36ca698e5ad16b561d446af12c0fd
Krishnan Mani <kmani@equalexperts.com>
2015-07-22 21:41:11 +0530

    Added example code for use of 'bang'; also re-formatted operators.js

--- app/config/operators.js
+++ app/config/operators.js
@@ -1,53 +1,104 @@
 module.exports = [
-  {"name": "hashrocket",
-   "symbol": "=>"},
-  {"name": "spaceship",
-   "symbol": "<=>"},
-  {"name": "threequals",
-   "symbol": "==="},
-  {"name": "twiddle wakka",
-   "symbol": "~>"},
-  {"name": "stabby lambda",
-   "symbol": "->"},
-  {"name": "spiral",
-   "symbol": "@"},
-  {"name": "splat",
-   "symbol": "*"},
-  {"name": "starargs",
-   "symbol": "*args"},
-  {"name": "tilde",
-   "symbol": "~"},
-  {"name": "octothorpe",
-   "symbol": "#"},
-  {"name": "skull tag",
-   "symbol": "<%="},
-  {"name": "bang",
-   "symbol": "!"},
-  {"name": "crab claws",
-   "symbol": "#{}",
-   "example": "# String interpolation\nputs \"1 + 2 = #{1 + 2}\"\n\n1 + 2 = 3"},
-  {"name": "curly",
-   "symbol": "{}  "},
-  {"name": "paren",
-   "symbol": "()"},
-  {"name": "bracket",
-   "symbol": "[]"},
-  {"name": "elvis",
-   "symbol": "?:"},
-  {"name": "shovel",
-   "symbol": "<<"},
-  {"name": "constellation",
-   "symbol": "**"},
-  {"name": "equalike",
-   "symbol": "=~"},
-  {"name": "hat",
-   "symbol": "^"},
-  {"name": "pretzel",
-   "symbol": "&"},
-  {"name": "whack",
-   "symbol": "/"},
-  {"name": "blank",
-   "symbol": "_"},
-  {"name": "t-square",
-   "symbol": "||="}
+    {
+        "name": "hashrocket",
+        "symbol": "=>"
+    },
+    {
+        "name": "spaceship",
+        "symbol": "<=>"
+    },
+    {
+        "name": "threequals",
+        "symbol": "==="
+    },
+    {
+        "name": "twiddle wakka",
+        "symbol": "~>"
+    },
+    {
+        "name": "stabby lambda",
+        "symbol": "->"
+    },
+    {
+        "name": "spiral",
+        "symbol": "@"
+    },
+    {
+        "name": "splat",
+        "symbol": "*"
+    },
+    {
+        "name": "starargs",
+        "symbol": "*args"
+    },
+    {
+        "name": "tilde",
+        "symbol": "~"
+    },
+    {
+        "name": "octothorpe",
+        "symbol": "#"
+    },
+    {
+        "name": "skull tag",
+        "symbol": "<%="
+    },
+    {
+        "name": "bang",
+        "symbol": "!",
+        "example": "# modify in-place\ny = [3, 5, 2]\nx = y.sort\np \"x is y sorted: #{x}, but y is still: #{y}\"\n# => \"x is y sorted: [2, 3, 5], but y is still: [3, 5, 2]\"\n\ny.sort!\np \"y is now sorted in-place: #{y}\"\n#  => \"y sorted in-place: [2, 3, 5]\""
+    },
+    {
+        "name": "crab claws",
+        "symbol": "#{}",
+        "example": "# String interpolation\nputs \"1 + 2 = #{1 + 2}\"\n\n1 + 2 = 3"
+    },
+    {
+        "name": "curly",
+        "symbol": "{}  "
+    },
+    {
+        "name": "paren",
+        "symbol": "()"
+    },
+    {
+        "name": "bracket",
+        "symbol": "[]"
+    },
+    {
+        "name": "elvis",
+        "symbol": "?:"
+    },
+    {
+        "name": "shovel",
+        "symbol": "<<"
+    },
+    {
+        "name": "constellation",
+        "symbol": "**"
+    },
+    {
+        "name": "equalike",
+        "symbol": "=~"
+    },
+    {
+        "name": "hat",
+        "symbol": "^"
+    },
+    {
+        "name": "pretzel",
+        "symbol": "&"
+    },
+    {
+        "name": "whack",
+        "symbol": "/"
+    },
+    {
+        "name": "blank",
+        "symbol": "_"
+    },
+    {
+        "name": "t-square",
+        "symbol": "||="
+    }
 ];
