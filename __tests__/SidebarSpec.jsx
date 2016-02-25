468c86aec8fca5631b735657468bf65da43709bb
Anil Wadghule <anildigital@gmail.com>
2016-02-25 16:42:01 +0530

    Fix jest test

--- __tests__/SidebarSpec.jsx
+++ __tests__/SidebarSpec.jsx
@@ -1,25 +1,26 @@
-jest.dontMock("../app/sidebar");
+jest.dontMock('../app/sidebar.jsx');
 jest.dontMock("../app/config/operators.js");
 
-describe("Sidebar", function() {
+var React = require('react');
+var ReactDOM = require('react-dom');
+var TestUtils = require('react-addons-test-utils');
+var Sidebar = require("../app/sidebar");
+var operators = require("../app/config/operators.js");
 
-    it("should render sidebar with expected operators", function() {
-        var React = require("react/addons");
-        var TestUtils = React.addons.TestUtils;
-
-        var Sidebar = require("../app/sidebar");
-        var operators = require("../app/config/operators.js");
-
-        var getCurrentOperator = jest.genMockFunction();
-
-        var selectOperator = jest.genMockFunction();
-
-        var sidebar = TestUtils.renderIntoDocument(
-            <Sidebar getCurrentOperator={getCurrentOperator} selectOperator={selectOperator} operators={operators}/>
+describe('Sidebar', () => {
+    it('should render sidebar with expected operator', () => {
+        const getCurrentOperator = jest.genMockFunction();
+        const selectOperator = jest.genMockFunction();
+        let sidebar = TestUtils.renderIntoDocument(
+            <Sidebar
+                getCurrentOperator={getCurrentOperator}
+                selectOperator={selectOperator}
+                operators={operators}/>
         );
-
-        var sidebar = TestUtils.findRenderedDOMComponentWithClass(sidebar, 'sidebar');
-        var html = sidebar.getDOMNode().textContent;
-        expect(html).toContain('hashrocket');
+        let sidebarNode = ReactDOM.findDOMNode(sidebar, 'Sidebar');
+        const html = sidebarNode.textContent;
+        operators.forEach(function (operator) {
+            expect(html).toContain(operator.name);
+        });
     });
 });
